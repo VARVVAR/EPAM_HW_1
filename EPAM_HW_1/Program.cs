12f0674f23bcad867f445a001836d993183e409c
user <user@DESKTOP-72FSFB0>
2024-03-07 23:37:53 +0200

    Add project files.

---
+++ EPAM_HW_1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPAM_HW_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Select the number of notebooks: ");
+            int NtBook = int.Parse(Console.ReadLine());
+
+            Console.Write("Select value of the notebook: ");
+            float ValueBook = float.Parse(Console.ReadLine());
+
+            Console.Write("Select the number of pens: ");
+            int Pen = int.Parse(Console.ReadLine());
+
+            Console.Write("Select value of the pen: ");
+            float ValuePen = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine($"quantity notebooks: {NtBook}\t Cost notebooks: {ValueBook * NtBook}$\nquantity pens: {Pen}\t Cost Pens: {ValuePen * Pen}$");
+            //Console.WriteLine($"Total quantity of Product: {NtBook + Pen}");
+            Console.WriteLine($"\nTotal amount: {ValueBook * NtBook + ValuePen * Pen}$");
+            Console.Read();
+
+
+
+        }
+    }
+}
